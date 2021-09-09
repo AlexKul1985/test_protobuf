f2ddcf8214dd3dc0ac4fb8a83ebd0f9c31a79973
akulikov <akulikov@stc-spb.ru>
2021-09-09 03:27:44 +0300

    ..

--- client/index.js
+++ client/index.js
@@ -1,6 +1,19 @@
-const o = require('./output/Account_pb')
+import o from './output/Account_pb';
 
-// const socket = new WebSocket("ws://localhost:8999");
+const path = document.getElementById("path");
+
+const socket = new WebSocket(path.value);
+
+socket.onopen = async function (t) {
+    console.log('Соединение установлено!');
+};
+
+socket.onmessage = async function (i) {
+    const s = await i.data.arrayBuffer()
+    const g = new Uint8Array(s);
+    const u = o.Account.deserializeBinary(g).toObject();
+    console.log('Received:',u);
+};
 
 const myObject = {
     name: "ame ame",
@@ -9,56 +22,18 @@ const myObject = {
     isNew: !1
 };
 
-var counter = document.getElementById("counter");
-var timer = document.getElementById("timer");
-var path = document.getElementById("path");
-document.getElementById("send-proto").addEventListener("click", (async function () {
-    var start, count = 0, times = counter.value, socket = new WebSocket(path.value);
-    socket.onopen = async function (t) {
-        // socket.send("proto"),
-        // start = Date.now();
-        var account = new o.Account;
+
+
+document.getElementById("send-proto").addEventListener("click",  function () {
+        const account = new o.Account;
+
         account.setName(myObject.name),
         account.setId(myObject.id),
         account.setDatetime(myObject.datetime),
         account.setIsnew(myObject.isNew);
-        var s = account.serializeBinary();
+
+        const s = account.serializeBinary();
         console.log('Send:', s);
         socket.send(s)
-    };
-
-    socket.onmessage = async function (i) {
-        var g = new Uint8Array(i), u = o.Account.deserializeBinary(g).toObject();
-        console.log('Rec:',u);
-        // if (count < times) {
-        //     var l = new o.Account;
-        //     l.setName(u.name),
-        //         l.setId(u.id),
-        //         l.setDatetime(u.datetime),
-        //         l.setIsnew(u.isNew);
-        //     var c = l.serializeBinary();
-        //     socket.send(c),
-        //         count++
-        // } else
-        //     timer.innerHTML = Date.now() - start + "ms.",
-                // socket.close()
-    };
-}
-))
-
-// document.forms.publish.onsubmit = function() {
-//     let outgoingMessage = this.message.value;
-  
-//     socket.send(outgoingMessage);
-//     return false;
-//   };
-  
-//   // получение сообщения - отобразить данные в div#messages
-//   socket.onmessage = function(event) {
-//     let message = event.data;
-  
-//     let messageElem = document.createElement('div');
-//     messageElem.textContent = message;
-//     document.getElementById('messages').prepend(messageElem);
-//   }
-// wsSend({name: 'Alex'})
+    }
+)
--- server.js
+++ server.js
@@ -5,40 +5,34 @@ const o = require('./client/output/Account_pb')
 
 
 const app = express();
-// app.use(cors)
+
 const server = http.createServer(app);
 
 const webSocketServer = new WebSocket.Server({ server });
 
+const myObject = {
+    name: "ame ame",
+    id: 69420,
+    datetime: 1611661486,
+    isNew: !1
+};
+
 webSocketServer.on('connection', ws => {
     ws.on('message', m => {
         const g = new Uint8Array(m);
-        const data = o.Account.deserializeBinary(g)
-        console.log(data.getName());
-        console.log(data.getId());
-        console.log(data.getIsnew());
-        console.log(data.getDatetime());
-
-        const d = {
-            name: data.getName(),
-            id: data.getId(),
-            isNew: data.getIsnew(),
-            datetime: data.getDatetime()
-        }
-        
-        var account = new o.Account;
+        const data = o.Account.deserializeBinary(g).toObject()
+        console.log(data);
+        const account = new o.Account;
         account.setName(data.name),
         account.setId(data.id),
         account.setDatetime(data.datetime),
         account.setIsnew(data.isnew)
-        var s = account.serializeBinary();
+        const s = account.serializeBinary();
         ws.send(s)
-        // webSocketServer.clients.forEach(client => client.send(m.toString()));
    });
 
    ws.on("error", e => ws.send(e));
-
-   ws.send('Hi there, I am a WebSocket server');
+  
 });
 
 server.listen(8999, () => console.log("Server started"))
